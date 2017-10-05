fe4088ff836018384ec6b3cf93eb2edf2b5248a0
DAN CHENG <dcheng0104@gmail.com>
2017-10-04 21:59:26 -0700

    more progress are made

--- public/scripts/controllers/recipeDetailController.js
+++ public/scripts/controllers/recipeDetailController.js
@@ -1,11 +1,20 @@
 angular.module('app')
 .controller('RecipeDetailController', function($scope, $location,dataService) {
 
-  	const recipeId = $location.url().split(':')[1];
+    if ($location.url().indexOf('add') >-1){
+      $scope.new=true;
+      const recipeId = $location.url().split(':')[1];
+    }
+    else{
+      $scope.new=false;
+    }
+  	
+    //displayName
+
 	
-	dataService.editRecipe(function(response) { 
-	    $scope.recipe = response.data;
-	},recipeId);
+  	dataService.editRecipe(function(response) { 
+  	    $scope.recipe = response.data;
+  	},recipeId);
 
     dataService.getCategories(function(response) { 
     	$scope.categories = response.data;
@@ -15,4 +24,21 @@ angular.module('app')
     	console.log(response.data);
     	$scope.foodItems = response.data;
   	}); 
+
+    $scope.removeIngredient = function(ingredient,$index){
+      $scope.recipe.ingredients.splice($index, 1);
+    }
+
+    $scope.addIngredient = function() {
+      const ingredient = {foodItem: "",condition:"",ammount:""};
+      $scope.recipe.ingredients.push(ingredient);
+    };
+
+    $scope.removeStep = function(step,$index){
+      $scope.recipe.steps.splice($index, 1);
+    };
+    $scope.addStep = function() {
+      const step = {description: ""};
+      $scope.recipe.steps.push(step);
+    };
 })
